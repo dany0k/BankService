85868f2d52eecdc9537640cb5ae7164c48a68de0
Den-Laptop <zmaev.vsu@gmail.com>
2023-03-10 19:12:16 +0300

    First commit

---
+++ BankService/src/main/java/ru/vsu/cs/zmaev/bankservice/BankServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.zmaev.bankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankServiceApplication.class, args);
+	}
+
+}
---
+++ BankService/src/test/java/ru/vsu/cs/zmaev/bankservice/BankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.zmaev.bankservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
